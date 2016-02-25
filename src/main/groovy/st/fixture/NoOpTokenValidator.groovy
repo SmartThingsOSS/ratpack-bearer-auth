6b7801a9159830c9b38f49c21563bdb39905530d
Mike Robinet <mike.robinet@smartthings.com>
2016-02-24 18:36:12 -0600

    Add NoOpTokenValidator for use in testing token authenticated endpoints

---
+++ src/main/groovy/st/fixture/NoOpTokenValidator.groovy
@@ -0,0 +1,21 @@
+package st.fixture
+
+import ratpack.exec.Promise
+import st.ratpack.auth.OAuthToken
+import st.ratpack.auth.TokenValidator
+import st.ratpack.auth.User
+
+/**
+ * Used to circumvent token validation during testing.
+ */
+class NoOpTokenValidator implements TokenValidator {
+
+	@Override
+	Promise<Optional<OAuthToken>> validate(String token) {
+		if (token.contains("service")) {
+			return Promise.value(Optional.of(new OAuthToken(Optional.<User> empty(), ['service'] as Set<String>, 'fake client')))
+		}
+		def user = Optional.of(new User("fakeUser", ["ROLE_FAKE"] as Set<String>))
+		return Promise.value(Optional.of(new OAuthToken(user, ['mobile'] as Set<String>, 'fake client')))
+	}
+}

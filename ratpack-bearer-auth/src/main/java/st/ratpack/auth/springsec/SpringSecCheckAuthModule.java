637b47869a0ad45b91d9c5ee1dfa85e26f6ee78f
dylanbijnagte <Dylan.Bijnagte@smartthings.com>
2018-03-13 15:56:51 -0500

    improve token caching by allowing configuration of cache time, add promise to cache prior to executing, allow refresh loading so there is never a cache miss, and disable cache config

--- ratpack-bearer-auth/src/main/java/st/ratpack/auth/springsec/SpringSecCheckAuthModule.java
+++ ratpack-bearer-auth/src/main/java/st/ratpack/auth/springsec/SpringSecCheckAuthModule.java
@@ -1,28 +1,34 @@
 package st.ratpack.auth.springsec;
 
-import com.google.inject.*;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Scopes;
 import com.google.inject.multibindings.OptionalBinder;
+import ratpack.exec.ExecController;
 import ratpack.guice.ConfigurableModule;
-import ratpack.http.client.HttpClient;
-import st.ratpack.auth.*;
+import st.ratpack.auth.CachingTokenValidator;
+import st.ratpack.auth.TokenProvider;
+import st.ratpack.auth.TokenValidator;
 
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 public class SpringSecCheckAuthModule extends ConfigurableModule<SpringSecCheckAuthModule.Config> {
 
 	@Override
 	protected void configure() {
 		OptionalBinder.newOptionalBinder(binder(), TokenProvider.class)
-			.setDefault().to(SpringSecCheckTokenProvider.class).in(Scopes.SINGLETON);
+				.setDefault().to(SpringSecCheckTokenProvider.class).in(Scopes.SINGLETON);
 
 		OptionalBinder.newOptionalBinder(binder(), TokenValidator.class)
-			.setDefault().toProvider(TokenValidatorProvider.class).in(Scopes.SINGLETON);
+				.setDefault().toProvider(TokenValidatorProvider.class).in(Scopes.SINGLETON);
 	}
 
 	public static class Config {
 		private URI host;
 		private String user;
 		private String password;
+		private CacheConfig cacheConfig = new CacheConfig();
 
 		public URI getHost() {
 			return host;
@@ -47,20 +53,70 @@ public class SpringSecCheckAuthModule extends ConfigurableModule<SpringSecCheckA
 		public void setPassword(String password) {
 			this.password = password;
 		}
+
+		public CacheConfig getCacheConfig() {
+			return cacheConfig;
+		}
+
+		public void setCacheConfig(CacheConfig cacheConfig) {
+			this.cacheConfig = cacheConfig;
+		}
+	}
+
+	public static class CacheConfig {
+		private boolean enabled = true;
+		private long cacheMinutes = 5L;
+		private boolean refresh = false;
+
+		public boolean isEnabled() {
+			return enabled;
+		}
+
+		public void setEnabled(boolean enabled) {
+			this.enabled = enabled;
+		}
+
+		public long getCacheMinutes() {
+			return cacheMinutes;
+		}
+
+		public void setCacheMinutes(long cacheMinutes) {
+			this.cacheMinutes = cacheMinutes;
+		}
+
+		public boolean isRefresh() {
+			return refresh;
+		}
+
+		public void setRefresh(boolean refresh) {
+			this.refresh = refresh;
+		}
 	}
 
 	public static class TokenValidatorProvider implements Provider<TokenValidator> {
 
 		private final TokenProvider tokenProvider;
+		private final CacheConfig cacheConfig;
+		private final ExecController controller;
 
 		@Inject
-		TokenValidatorProvider(TokenProvider tokenProvider) {
+		TokenValidatorProvider(TokenProvider tokenProvider, Config config, ExecController controller) {
 			this.tokenProvider = tokenProvider;
+			this.cacheConfig = config.getCacheConfig();
+			this.controller = controller;
 		}
 
 		@Override
 		public TokenValidator get() {
-			return new CachingTokenValidator(new SpringSecCheckTokenValidator(tokenProvider));
+			TokenValidator validator = new SpringSecCheckTokenValidator(tokenProvider);
+			if (cacheConfig.isEnabled()) {
+				validator = new CachingTokenValidator(
+						validator,
+						cacheConfig.getCacheMinutes(), TimeUnit.MINUTES,
+						cacheConfig.isRefresh(),
+						controller);
+			}
+			return validator;
 		}
 	}
 
--- ratpack-bearer-auth/src/test/groovy/st/ratpack/auth/CachingTokenValidatorSpec.groovy
+++ ratpack-bearer-auth/src/test/groovy/st/ratpack/auth/CachingTokenValidatorSpec.groovy
@@ -5,6 +5,10 @@ import ratpack.exec.Promise
 import ratpack.test.exec.ExecHarness
 import spock.lang.AutoCleanup
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
 
 class CachingTokenValidatorSpec extends Specification {
 
@@ -15,14 +19,14 @@ class CachingTokenValidatorSpec extends Specification {
 		given:
 		String token = "fakeToken"
 		OAuthToken oAuthToken =
-				new DefaultOAuthToken.Builder()
-					.setAuthToken(token)
-					.build()
+			new DefaultOAuthToken.Builder()
+				.setAuthToken(token)
+				.build()
 
 		TokenValidator tokenValidator = Mock(TokenValidator)
 		CachingTokenValidator cachingTokenValidator
-		harness.run {
-			cachingTokenValidator = new CachingTokenValidator(tokenValidator)
+		harness.run { exec ->
+			cachingTokenValidator = new CachingTokenValidator(tokenValidator, 2, TimeUnit.SECONDS, false, exec.controller)
 		}
 
 		when: "Validating a token"
@@ -41,9 +45,130 @@ class CachingTokenValidatorSpec extends Specification {
 		}
 
 		then: "No More calls to upstream"
-		0 * _._
+		0 * tokenValidator.validate(_)
 		result.success
 		result.value.isPresent()
+
+		when: "validate after timeout"
+		result = harness.yield {
+			sleep(2000)
+			cachingTokenValidator.validate(token)
+		}
+
+		then: "Call upstream again"
+		1 * tokenValidator.validate(token) >> Promise.<Optional<OAuthToken>> value(Optional.<OAuthToken> of(oAuthToken))
+		result.success
+		result.value.isPresent()
+	}
+
+	def "Caching validator returns old value while refreshing"() {
+		given:
+		AtomicInteger counter = new AtomicInteger(0)
+		String token = "fakeToken"
+
+		TokenValidator tokenValidator = Mock()
+
+		def conditions = new PollingConditions(timeout: 10)
+
+		_ * tokenValidator.validate(token) >> { t ->
+			int count = counter.incrementAndGet()
+			if (count < 3) {
+				Promise.value(Optional.of(
+					new DefaultOAuthToken.Builder()
+						.setAuthToken(token)
+						.setAdditionalInformation([version: count])
+						.build()))
+			} else {
+				Promise.value(Optional.empty())
+			}
+		}
+
+		CachingTokenValidator cachingTokenValidator
+
+		harness.run { exec ->
+			cachingTokenValidator = new CachingTokenValidator(tokenValidator, 1, TimeUnit.SECONDS, true, exec.controller)
+		}
+
+		when: "Validating a token"
+		ExecResult<Optional<OAuthToken>> result = harness.yield {
+			cachingTokenValidator.validate(token)
+		}
+
+		then: "Calls upstream on a cache miss"
+		conditions.eventually {
+			counter.get() == 1
+		}
+
+		result.success
+		result.value.isPresent()
+		result.value.get().additionalInformation.version == 1
+
+		when: "A second validation call happens"
+		result = harness.yield {
+			cachingTokenValidator.validate(token)
+		}
+
+		then: "No More calls to upstream"
+		result.success
+		result.value.isPresent()
+		result.value.get().additionalInformation.version == 1
+
+		sleep(1111) // wait for possible async call and timeout of cache
+		counter.get() == 1 // still not called
+
+		when: "validate after timeout"
+		result = harness.yield {
+			cachingTokenValidator.validate(token)
+		}
+
+		then: "Call upstream again but still present during refresh"
+		result.success
+		result.value.isPresent()
+		result.value.get().additionalInformation.version == 1
+
+		conditions.eventually { // the token should be fetched eventually
+			counter.get() == 2
+		}
+
+		when: "the cache should now return the refreshed value"
+		sleep(10) // wait just a bit after the increment for the promise to be put in the cache
+		result = harness.yield {
+			cachingTokenValidator.validate(token)
+		}
+
+		then:
+		result.success
+		result.value.isPresent()
+		result.value.get().additionalInformation.version == 2
+		sleep(1111) // wait for possible async call and timeout of cache
+		counter.get() == 2 // still not called
+
+		when: 'trigger another refresh'
+		result = harness.yield {
+			cachingTokenValidator.validate(token)
+		}
+
+		then: 'get the cached value while it is refreshed'
+		result.success
+		result.value.isPresent()
+		result.value.get().additionalInformation.version == 2
+
+		when: 'the cache is populated with an empty token and then is fetched'
+		conditions.eventually { // the token should be fetched eventually
+			counter.get() == 3
+		}
+		sleep(10) // wait just a bit after the increment for the promise to be put in the cache
+		result = harness.yield {
+			cachingTokenValidator.validate(token)
+		}
+
+		then: 'the result is now empty and an immediate call to refresh happens without waiting for the timeout'
+		result.success
+		!result.value.isPresent()
+
+		conditions.eventually { // the token should be fetched eventually
+			counter.get() == 4
+		}
 	}
 
 }

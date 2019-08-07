01c34049037f4d7f630a31a09b8b5adc2a5b98a0
Arun Parthiban <arunparthiban@MN-APARTHIBAN-M.local>
2019-08-07 11:52:35 -0500

    Export timer metrics when calling check_token

--- ratpack-bearer-auth/src/main/java/st/ratpack/auth/springsec/SpringSecCheckTokenProvider.java
+++ ratpack-bearer-auth/src/main/java/st/ratpack/auth/springsec/SpringSecCheckTokenProvider.java
@@ -1,16 +1,20 @@
 package st.ratpack.auth.springsec;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.google.inject.Inject;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import ratpack.exec.Promise;
+import ratpack.func.Action;
 import ratpack.http.HttpUrlBuilder;
 import ratpack.http.client.HttpClient;
 import ratpack.http.client.ReceivedResponse;
-import ratpack.http.client.RequestSpec;
 import st.ratpack.auth.TokenProvider;
 
 import java.net.URI;
+import java.time.Duration;
 import java.util.Base64;
+import java.util.concurrent.TimeUnit;
 
 import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON;
 import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED;
@@ -19,14 +23,19 @@ public class SpringSecCheckTokenProvider implements TokenProvider {
 
 	private final HttpClient httpClient;
 	private final SpringSecCheckAuthModule.Config config;
+	private final Action<Duration> checkTokenTimer;
 
 	@Inject
 	public SpringSecCheckTokenProvider(
 		HttpClient httpClient,
-		SpringSecCheckAuthModule.Config config
+		SpringSecCheckAuthModule.Config config,
+		MetricRegistry metricRegistry
+
 	) {
 		this.httpClient = httpClient;
 		this.config = config;
+		Timer timer = metricRegistry.timer("oauth.check_token");
+		this.checkTokenTimer = duration -> timer.update(duration.toMillis(), TimeUnit.MILLISECONDS);
 	}
 
 	@Override
@@ -46,8 +55,7 @@ public class SpringSecCheckTokenProvider implements TokenProvider {
 					buildBasicAuthHeader(config.getUser(), config.getPassword())
 				);
 				headers.add(HttpHeaderNames.ACCEPT, APPLICATION_JSON);
-			})
-		);
+			})).time(checkTokenTimer);
 	}
 
 	private String buildBasicAuthHeader(String user, String password) {
